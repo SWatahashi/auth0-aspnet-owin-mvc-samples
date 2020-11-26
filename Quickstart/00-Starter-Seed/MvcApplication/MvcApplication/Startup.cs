0caa412ccf65d57eabf4a61ef707e153e4600d45
swatahashi <swatahashi@contentsworks.co.jp>
2020-11-26 12:15:43 +0900

    CookieミドルウェアとAuth0ミドルウェアの構成の追加 https://auth0.com/docs/quickstart/webapp/aspnet-owin#install-and-configure-the-openid-connect-middleware

--- Quickstart/00-Starter-Seed/MvcApplication/MvcApplication/Startup.cs
+++ Quickstart/00-Starter-Seed/MvcApplication/MvcApplication/Startup.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
 using Microsoft.Owin.Host.SystemWeb;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 using MvcApplication.Support;
 using Owin;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
 
 [assembly: OwinStartup(typeof(MvcApplication.Startup))]
 
@@ -13,15 +19,75 @@ namespace MvcApplication
     {
         public void Configuration(IAppBuilder app)
         {
+            // Configure Auth0 parameters
+            string auth0Domain = ConfigurationManager.AppSettings["auth0:Domain"];
+            string auth0ClientId = ConfigurationManager.AppSettings["auth0:ClientId"];
+            string auth0ClientSecret = ConfigurationManager.AppSettings["auth0:ClientSecret"];
+            string auth0RedirectUri = ConfigurationManager.AppSettings["auth0:RedirectUri"];
+            string auth0PostLogoutRedirectUri = ConfigurationManager.AppSettings["auth0:PostLogoutRedirectUri"];
+
             // Set Cookies as default authentication type
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
                 LoginPath = new PathString("/Account/Login"),
-                CookieSameSite = SameSiteMode.Lax
+
+                // Configure SameSite as needed for your app. Lax works well for most scenarios here but
+                // you may want to set SameSiteMode.None for HTTPS
+                CookieSameSite = SameSiteMode.Lax,
             });
 
+            // Configure Auth0 authentication
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationType = "Auth0",
+
+                Authority = $"https://{auth0Domain}",
+
+                ClientId = auth0ClientId,
+                ClientSecret = auth0ClientSecret,
+
+                RedirectUri = auth0RedirectUri,
+                PostLogoutRedirectUri = auth0PostLogoutRedirectUri,
+
+                ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                Scope = "openid profile",
+
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name"
+                },
+
+                CookieManager = new SameSiteCookieManager(new SystemWebCookieManager()),
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    RedirectToIdentityProvider = notification =>
+                    {
+                        if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.Logout)
+                        {
+                            var logoutUri = $"https://{auth0Domain}/v2/logout?client_id={auth0ClientId}";
+
+                            var postLogoutUri = notification.ProtocolMessage.PostLogoutRedirectUri;
+                            if (!string.IsNullOrEmpty(postLogoutUri))
+                            {
+                                if (postLogoutUri.StartsWith("/"))
+                                {
+                                    // transform to absolute
+                                    var request = notification.Request;
+                                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                                }
+                                logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                            }
+
+                            notification.Response.Redirect(logoutUri);
+                            notification.HandleResponse();
+                        }
+                        return Task.FromResult(0);
+                    }
+                }
+            });
         }
     }
 }
